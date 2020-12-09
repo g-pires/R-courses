6f62ce61187c6eaa75c7a6a47e2126808af24daf
glageosa <glageosa@gmail.com>
2020-12-09 17:14:37 +0100

    modified scripts

--- test.R
+++ test.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(ggplot2)
 
-ggplot(data=mpg, aes(x=displ, y=hwy))+
-  geom_point()+
-  geom_function()
+ggplot(data=mpg, aes(x=class, y=hwy))+
+  geom_boxplot()+
+  stat_compare_means(aes(label = ..p.signif..), 
+                     label.x = 1.5, label.y = 40, ref.group=".all.")
